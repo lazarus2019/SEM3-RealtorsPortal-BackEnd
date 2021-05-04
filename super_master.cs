9549270dae17bc536eca20fc11796885a8f7d0d2
NTSon <thaisonbk2020@gmail.com>
2021-05-04 20:01:47 +0700

    Upload file super_master.cs

---
+++ super_master.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Super_Master
+{
+	public Super_Master()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
