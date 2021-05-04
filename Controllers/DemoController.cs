f2d6f7a4ca06888a8780977cc0b391fcb7289e17
NTSon <thaisonbk2020@gmail.com>
2021-05-04 20:16:34 +0700

    Upload template  ASP.NET API SEM3 RealtorsPortal

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,35 @@
+﻿using ASP_APIServer1.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_APIServer1.Controllers
+{
+	[Route("api/demo")]
+	public class DemoController : Controller
+	{
+		private DemoService demoService;
+
+		public DemoController(DemoService _demoService)
+		{
+			demoService = _demoService;
+		}
+
+		[Produces("application/json")]
+		[HttpGet("demo1")]
+		public IActionResult Demo1()
+		{
+			try
+			{
+				return Ok();
+			}
+			catch
+			{
+				return BadRequest();
+			}
+		}
+
+	}
+}
---
+++ Services/DemoService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_APIServer1.Services
+{
+	public interface DemoService
+	{
+	}
+}
