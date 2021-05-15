8442f149068ef141a4b2cbde5e7d1139aa53f863
VietNguyen <vietnguyen0444@gmail.com>
2021-05-15 10:22:22 +0700

    add autoMapperProfile

---
+++ NETAPI_SEM3/Data/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using NETAPI_SEM3.Entities;
+using NETAPI_SEM3.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETAPI_SEM3.Data
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<MemberViewModel, Member>().ReverseMap();
+            CreateMap<PropertyViewModel, Property>().ReverseMap();
+        }
+    }
+}
