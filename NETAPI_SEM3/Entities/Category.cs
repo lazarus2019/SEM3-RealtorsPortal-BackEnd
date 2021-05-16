51879c437804b93f74e9814646fa57a4c4c984fc
VietNguyen <vietnguyen0444@gmail.com>
2021-05-16 22:34:58 +0700

    Entity model from DB

---
+++ NETAPI_SEM3/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NETAPI_SEM3.Entities
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            CustomerContacts = new HashSet<CustomerContact>();
+            Properties = new HashSet<Property>();
+        }
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<CustomerContact> CustomerContacts { get; set; }
+        public virtual ICollection<Property> Properties { get; set; }
+    }
+}
---
+++ NETAPI_SEM3/Entities/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NETAPI_SEM3.Entities
+{
+    public partial class City
+    {
+        public City()
+        {
+            Properties = new HashSet<Property>();
+        }
+
+        public string CityId { get; set; }
+        public string Name { get; set; }
+        public string RegionId { get; set; }
+
+        public virtual Region Region { get; set; }
+        public virtual ICollection<Property> Properties { get; set; }
+    }
+}
