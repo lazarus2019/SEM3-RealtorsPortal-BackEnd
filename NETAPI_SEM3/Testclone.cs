a4d0dc33b7188e3748403819bf0b729683c115ef
NTSon <thaisonbk2020@gmail.com>
2021-05-16 09:50:30 +0700

    Revert "Test clone & merge success. Delete file class testclone.cs"
    This reverts commit 3a9801dbf1de6cedd67db4a7b7e4b4274ecc45f1.

---
+++ NETAPI_SEM3/Testclone.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETAPI_SEM3
+{
+	public class Testclone
+	{
+		private Testclone()
+		{
+
+		}
+
+		[Produces("application/json")]
+		[HttpGet("demo1")]
+		public IActionResult Demo1()
+		{
+			return null;
+		}
+	}
+}
