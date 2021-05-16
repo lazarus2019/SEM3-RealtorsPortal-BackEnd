3dc96949299e8e2e2b7b8cf6e52370390853498f
VietNguyen <vietnguyen0444@gmail.com>
2021-05-16 22:36:15 +0700

    Mail model for send mail

---
+++ NETAPI_SEM3/ViewModel/MailRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETAPI_SEM3.ViewModel
+{
+    public class MailRequest
+    {
+        public string email { get; set; }
+        public string subject { get; set; }
+        public string content { get; set; }
+    }
+}
