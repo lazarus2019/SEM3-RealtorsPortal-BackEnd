1052cd5d578e3bfbc2a9937c13203df60b16930f
VietNguyen <vietnguyen0444@gmail.com>
2021-05-16 22:31:54 +0700

    add more function for controller

--- NETAPI_SEM3/Controllers/AdsPackageController.cs
+++ NETAPI_SEM3/Controllers/AdsPackageController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NETAPI_SEM3.Entities;
+using NETAPI_SEM3.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,26 @@ namespace NETAPI_SEM3.Controllers
     [Route("api/adspackage")]
     public class AdsPackageController : Controller
     {
-        public IActionResult Index()
+        private readonly IMapper _mapper;
+        private readonly AdsPackageService _adsPackageService;
+
+        public AdsPackageController(AdsPackageService adsPackageService, IMapper mapper)
         {
-            return View();
+            this._adsPackageService = adsPackageService;
+            this._mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllAdsPackage()
+        {
+            try
+            {
+                return Ok(_adsPackageService.GetAllAdsPackage());
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- NETAPI_SEM3/Controllers/MemberController.cs
+++ NETAPI_SEM3/Controllers/MemberController.cs
@@ -7,6 +7,13 @@ using NETAPI_SEM3.Entities;
 using NETAPI_SEM3.ViewModel;
 using NETAPI_SEM3.Services;
 using AutoMapper;
+using DemoSession16.Helpers;
+using Microsoft.Extensions.Configuration;
+using System.Security.Authentication;
+using MailKit.Net.Smtp;
+using MailKit.Net.Pop3;
+using MailKit.Security;
+using System.Diagnostics;
 
 namespace NETAPI_SEM3.Controllers
 {
@@ -15,14 +22,15 @@ namespace NETAPI_SEM3.Controllers
     {
         private readonly MemberService _memberService;
         private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
 
-        public MemberController(MemberService memberService, IMapper mapper)
+        public MemberController(MemberService memberService, IMapper mapper, IConfiguration _configuration)
         {
             this._memberService = memberService;
             this._mapper = mapper;
+            this._configuration = _configuration;
         }
 
-        [Route("getallmember")]
         [HttpGet]
         public IActionResult GetAllMember()
         {
@@ -35,5 +43,24 @@ namespace NETAPI_SEM3.Controllers
                 return BadRequest();
             }
         }
+
+        [HttpPost("sendEmail")]
+        public IActionResult Send([FromBody] MailRequest mailRequest)
+        {
+            try
+            {
+                Debug.WriteLine("subject: " + mailRequest.subject);
+                Debug.WriteLine("subject: " + mailRequest.email);
+                var mailHelper = new MailHelper(_configuration);
+                mailHelper.Send(mailRequest.email, _configuration["Gmail:Username"], mailRequest.subject, mailRequest.content);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+
     }
 }
