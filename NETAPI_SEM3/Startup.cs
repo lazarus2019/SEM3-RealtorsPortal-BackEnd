7b6b0ebab10eb9d95229c19abafc7babac56ba72
Nguyen Tuan Anh <NTanh.vn@outlook.com>
2021-05-27 13:06:30 +0700

    update Startup 27/5

--- NETAPI_SEM3/Startup.cs
+++ NETAPI_SEM3/Startup.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -5,7 +6,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NETAPI_SEM3.Models;
 using NETAPI_SEM3.Services;
+using NETAPI_SEM3.Services.User;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,11 +29,18 @@ namespace NETAPI_SEM3
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+			services.AddControllers().AddNewtonsoftJson(options =>
+		options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
 			// Khai bao ket noi database
 			var connectionString = configuration.GetConnectionString("DefaultConnection");
-			//services.AddDbContext<MyDBContext>(option => option.UseLazyLoadingProxies().UseSqlServer(connectionString));
+			services.AddDbContext<DatabaseContext>(option => option.UseLazyLoadingProxies().UseSqlServer(connectionString));
 			services.AddScoped<DemoService, DemoServiceImpl>();
+			services.AddScoped<IndexService, IndexServiceImpl>();
+
+			services.AddScoped<SellerService, SellerServiceImpl>();
+			services.AddScoped<NewsBlogService, NewsBlogServiceImpl>();
+			services.AddScoped<AboutUsService, AboutUsServiceImpl>();
 		}
 
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -39,9 +49,17 @@ namespace NETAPI_SEM3
 			{
 				app.UseDeveloperExceptionPage();
 			}
+			app.UseCors(builder => builder
+				.AllowAnyHeader()
+				.AllowAnyMethod()
+				.SetIsOriginAllowed((host) => true)
+				.AllowCredentials()
+			);
+			app.UseHttpsRedirection();
 
 			app.UseRouting();
 			app.UseStaticFiles();
+
 
 			app.UseEndpoints(endpoints =>
 			{
