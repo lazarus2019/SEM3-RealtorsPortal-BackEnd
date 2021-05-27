2a197b2506c1798f8c01aef163036e413a34a9c6
Nguyen Tuan Anh <NTanh.vn@outlook.com>
2021-05-27 13:02:40 +0700

    update controller 27/5

---
+++ NETAPI_SEM3/Controllers/User/ListingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETAPI_SEM3.Controllers.User
+{
+    public class ListingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
