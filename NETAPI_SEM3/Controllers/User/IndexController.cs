4df1e3ec1f11465ce01cdbfe333859a70b15634b
Nguyen Tuan Anh <NTanh.vn@outlook.com>
2021-05-27 12:28:27 +0700

    Commit Controller/User : AboutUs, News, PhoneBook

---
+++ NETAPI_SEM3/Controllers/User/IndexController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NETAPI_SEM3.Models;
+using NETAPI_SEM3.Services.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETAPI_SEM3.Controllers.User
+{
+    [Route("api/user/")]
+    public class IndexController : Controller
+    {
+     
+        private IndexService homeService;
+        public IndexController(IndexService _homeService)
+        {
+            homeService = _homeService;
+        }
+
+
+        [Produces("application/json")]
+        [HttpGet("loadtopproperty")]
+        public IActionResult LoadTopProperty()
+        {
+            try
+            {
+                var properties = homeService.LoadTopProperty(); 
+                
+                return Ok(properties);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [Produces("application/json")]
+        [HttpGet("loadpopularlocation")]
+        public IActionResult LoadCategoriesNumber()
+        {
+            try
+            {
+                var categories = homeService.LoadPopularLocations(); 
+                
+                return Ok(categories);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ NETAPI_SEM3/Controllers/User/PhoneBookController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NETAPI_SEM3.Services.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETAPI_SEM3.Controllers.User
+{
+    [Route("api/user/")]
+    public class PhoneBookController : Controller
+    {
+
+        private SellerService sellerService;
+
+        public PhoneBookController(SellerService _sellerService)
+        {
+            sellerService = _sellerService;
+        }
+
+        [Produces("application/json")]
+        [HttpGet("loadseller")]
+        public IActionResult LoadSeller()
+        {
+            try
+            {
+                var seller = sellerService.LoadSeller();
+
+                return Ok(seller);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [Produces("application/json")]
+        [HttpGet("loadagent")]
+        public IActionResult LoadAgent()
+        {
+            try
+            {
+                var agent = sellerService.LoadAgent();
+
+                return Ok(agent);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [Produces("application/json")]
+        [HttpGet("sellerID/{sellerId}")]
+        public IActionResult SellerDetails(int sellerId)
+        {
+            try
+            {
+                var detail = sellerService.SellerDetails(sellerId);
+                return Ok(detail);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        
+        [Produces("application/json")]
+        [HttpGet("propertyID/{propertyId}")]
+        public IActionResult SellerPropertyId(int propertyId)
+        {
+            try
+            {
+                var propertyID = sellerService.LoadPropertyId(propertyId);
+                return Ok(propertyID);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
