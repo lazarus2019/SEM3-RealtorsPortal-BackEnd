a8bc228d1a172430a439e8e122d1d53c6cf8bda9
Nguyen Tuan Anh <NTanh.vn@outlook.com>
2021-06-03 11:44:28 +0700

    Update Entities

---
+++ NETAPI_SEM3/Entities/CategoryNews.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETAPI_SEM3.Entities
+{
+    public class CategoryNews
+    {
+        public int NewsId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Status { get; set; }
+        public string ThumbailName { get; set; }
+
+    }
+}
---
+++ NETAPI_SEM3/Entities/MemberNews.cs
@@ -0,0 +1,23 @@
+﻿using NETAPI_SEM3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETAPI_SEM3.Entities
+{
+    public class MemberNews
+    {
+        public int MemberId { get; set; }
+        public string RoleId { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        public string Photo { get; set; }
+        public DateTime CreateDate { get; set; }
+        
+
+    }
+}
